2673ab5273b6708cb3b841379d727beec663f57b
nicoletaudy <taudy.n@gmail.com>
2024-11-11 19:43:35 +0100

    osetreni kalkulacky vyjimkami

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,7 +1,18 @@
 def kalkulacka():
-    a = int(input("zadejte prvni cislo: "))
+    try:
+        a = int(input("zadejte prvni cislo: "))
+    except ValueError:
+        print("spatne zadana hodnota, ukoncuji program")
+        quit()
+
     operace = input("zadejte znamenko: ")
-    b = int(input("zadejte druhe cislo: "))
+
+    try:
+        b = int(input("zadejte druhe cislo: "))
+    except ValueError:
+        print("spatne zadana hodnota, ukoncuji program")
+        quit()
+
     if operace == "+":
         return a + b
     elif operace == "-":
@@ -13,5 +24,8 @@ def kalkulacka():
             return a // b
         except ZeroDivisionError:
             print("nelze delit nulou")
+    else:
+        print("spatne zadany operator, ukonuji program")
+        quit()
 
 print(kalkulacka())
