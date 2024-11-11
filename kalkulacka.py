1f47ec8b441f425a137a29705eff5267dae240d0
nicoletaudy <taudy.n@gmail.com>
2024-11-11 18:39:41 +0100

    zapis do readme

---
+++ kalkulacka.py
@@ -0,0 +1,14 @@
+def kalkulacka():
+    a = int(input("zadejte prvni cislo: "))
+    operace = input("zadejte znamenko: ")
+    b = int(input("zadejte druhe cislo: "))
+    if operace == "+":
+        return a + b
+    elif operace == "-":
+        return a - b
+    elif operace == "*":
+        return a * b
+    elif operace == "/":
+        return a / b
+
+print(kalkulacka())
